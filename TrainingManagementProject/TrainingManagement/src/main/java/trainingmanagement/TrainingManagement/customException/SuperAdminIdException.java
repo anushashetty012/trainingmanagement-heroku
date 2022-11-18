60e05a6e0846733c53987e8d8c8ad6b95ccef1bd
anushashetty012 <111722083+anushashetty012@users.noreply.github.com>
2022-11-18 12:07:54 +0530

    Delete TrainingManagementProject directory

--- TrainingManagementProject/TrainingManagement/src/main/java/trainingmanagement/TrainingManagement/customException/SuperAdminIdException.java
+++
@@ -1,9 +0,0 @@
-package trainingmanagement.TrainingManagement.customException;
-
-public class SuperAdminIdException extends Exception
-{
-    public SuperAdminIdException(String msg)
-    {
-        super(msg);
-    }
-}
